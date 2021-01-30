37c029de01193be44d4023051c114e3ab43320c5
AkihisaOmata <9an22aa@gmail.com>
2021-01-31 01:14:34 +0900

    素数の判定を追加

--- Test/main.cpp
+++ Test/main.cpp
@@ -1,11 +1,19 @@
 #include<stdio.h>
 int fib(int);
+bool IsPrime(int n);
 
 int main(void) {
 	printf("Hello World!\n");
 
 	for (int i = 0; i < 20; i++) {
-		printf("%i\n", fib(i));
+		int x = fib(i);
+		printf("%i ", x);
+		if (IsPrime(x)) {
+			printf("Prime!\n");
+		}
+		else {
+			printf("Not Prime\n");
+		}
 	}
 
 	return 0;
@@ -22,4 +30,13 @@ int fib(int n) {
 		return 1;
 	}
 	return fib(n - 1) + fib(n - 2);
+}
+
+bool IsPrime(int n) {
+	for (int i = 2; i < n; i++) {
+		if (n % i == 0) {
+			return false;
+		}
+	}
+	return true;
 }
